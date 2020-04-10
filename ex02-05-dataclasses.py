d81ae48936c34abb566c51027afb0121c34d516a
dgapitts <dgapitts@gmail.com>
2020-04-10 20:42:14 +0000

    starting with python dataclasses ... printing is fiddly

---
+++ ex02-05-dataclasses.py
@@ -0,0 +1,12 @@
+# https://realpython.com/python-data-classes/
+from dataclasses import dataclass
+
+@dataclass
+class DataClassCard:
+    rank: str
+    suit: str
+
+queen_of_hearts = DataClassCard('Q', 'Hearts')
+print("queen_of_hearts.rank:" + queen_of_hearts.rank)
+print("queen_of_hearts:")
+print(queen_of_hearts)
